1bb024aa43183e5f7b4749698089bdac6a2d32aa
PhamHuuMai <huumaipham1996@gmail.com>
2017-11-05 17:33:51 +0700

    no message

--- SendMail/src/main/java/vn/com/ntqsolution/server/controller/FortuneTellerVerifyCodeController.java
+++ SendMail/src/main/java/vn/com/ntqsolution/server/controller/FortuneTellerVerifyCodeController.java
@@ -3,7 +3,7 @@ package vn.com.ntqsolution.server.controller;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import vn.com.ntqsolution.server.worker.email.EmailDeliver;
+import vn.com.ntqsolution.worker.email.EmailDeliver;
 
 /**
  * @author TungLK
--- SendMail/src/main/java/vn/com/ntqsolution/worker/email/EmailDeliver.java
+++ SendMail/src/main/java/vn/com/ntqsolution/worker/email/EmailDeliver.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package vn.com.ntqsolution.server.worker.email;
+package vn.com.ntqsolution.worker.email;
 
 import java.util.Properties;
 import javax.mail.Authenticator;
